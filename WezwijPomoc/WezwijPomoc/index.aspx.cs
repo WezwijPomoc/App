5b96490a1b7313bbf8022c7b5fb5836536184022
hasebedej <radoslaw.kister@student.wat.edu.pl>
2020-03-19 01:20:06 +0100

    Walidacja pesel

--- WezwijPomoc/WezwijPomoc/index.aspx.cs
+++ WezwijPomoc/WezwijPomoc/index.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,61 @@ namespace WezwijPomoc
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        bool ValidatePesel()
+        {
+            string pesel = peselTextBox.Text;
+            int peselNum;
+            var pesel1 = pesel[0];
+            var pesel2 = pesel[1];
+            var pesel3 = pesel[2];
+            var pesel4 = pesel[3];
+            var pesel5 = pesel[4];
+            var pesel6 = pesel[5];
+            var pesel7 = pesel[6];
+            var pesel8 = pesel[7];
+            var pesel9 = pesel[8];
+            var pesel10 = pesel[9];
+            var pesel11 = pesel[10];
+
+            try
+            {
+                int peselInt1 = Int32.Parse(pesel1.ToString());
+                int peselInt2 = Int32.Parse(pesel2.ToString());
+                int peselInt3 = Int32.Parse(pesel3.ToString());
+                int peselInt4 = Int32.Parse(pesel4.ToString());
+                int peselInt5 = Int32.Parse(pesel5.ToString());
+                int peselInt6 = Int32.Parse(pesel6.ToString());
+                int peselInt7 = Int32.Parse(pesel7.ToString());
+                int peselInt8 = Int32.Parse(pesel8.ToString());
+                int peselInt9 = Int32.Parse(pesel9.ToString());
+                int peselInt10 = Int32.Parse(pesel10.ToString());
+                int peselInt11 = Int32.Parse(pesel11.ToString());
+                peselNum = peselInt1 * 1 + peselInt2 * 3 + peselInt3 * 7 + peselInt4 * 9 + peselInt5 * 1 + peselInt6 * 3 + peselInt7 * 7 + peselInt8 * 9 + peselInt9 * 1 + peselInt10 * 3;
+                peselNum = peselNum % 10;
+                peselNum = 10 - peselNum;
+                peselNum = peselNum % 10;
+                if (peselNum == peselInt11)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (FormatException error)
+            {
+                //strona błędu
+                return false;
+
+            }
+        }
+        protected void Submit(object sender, EventArgs e)
+        {
+            Debug.Write(ValidatePesel());
+            
         }
     }
 }
